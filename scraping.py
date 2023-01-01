ed4624d17e77416587602ddb5173ff37a7186e1e
Douglas Talbert <douglas.talbert@duke.edu>
2023-01-01 10:57:57 -0800

    initial summary scraping done

--- scraping.py
+++ scraping.py
@@ -10,7 +10,7 @@ current_year = 2015
 url = f"https://www.baseball-reference.com/leagues/MLB/{current_year}-schedule.shtml"
 resp = requests.get(url)
 soup = bs(resp.text)
-games = soup.findAll('a',text='Boxscore')
+games = soup.findAll('a', text='Boxscore')
 game_links.extend([x['href'] for x in games])
 print("Number of games to download: ", len(game_links))
 game_links[0]
@@ -23,14 +23,16 @@ game_id = url.split('/')[-1][:-6]
 game_id
 soup = bs(response.text)
 
-    #Game Summary
-game_summary = {'game_id' : game_id}
-scorebox = soup.find('div', {'class':'scorebox'})
+#Game Summary
+game_summary = {'game_id': game_id}
+scorebox = soup.find('div', {'class': 'scorebox'})
 strongs = scorebox.find_all('strong')
-strongs
 game_summary['away_team_abbr'] = strongs[2]['href'].split('/')[-2]
 game_summary['home_team_abbr'] = strongs[6]['href'].split('/')[-2]
+meta = scorebox.find('div', {'class': 'scorebox_meta'}).find_all('div')
+#TODO: Add additional summary info for total / score forecasting
+game_summary['date'] = meta[0].text.strip()
+game_summary['start_time'] = meta[1].text[12:-6].strip()
 game_summary
-teams = strongs.find_all('a')
 teams
 ##########
